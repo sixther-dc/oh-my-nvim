0882c7249c50dc856e7ff54818fe18adbe73beca
sixther-dc <382998946@qq.com>
2023-07-05 14:35:55 +0800

    优化gopls

--- lua/lsp/config/go.lua
+++ lua/lsp/config/go.lua
@@ -1,16 +1,28 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+util = require "lspconfig/util"
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
 local opts = {
+  cmd = {"gopls", "serve"},
+  filetypes = {"go", "gomod"},
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+    },
+  },
 }
 
 -- 查看目录等信息
 return {
-    on_setup = function(server)
-        server:setup(opts)
-    end,
+  on_setup = function(server)
+    server:setup(opts)
+  end,
 }
 
 
